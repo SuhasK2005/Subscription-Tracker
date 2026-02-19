15b4e1c2598cd23c5f62003e1d92034b3b463c1a
SuhasK2005 <saisuhask2005@gmail.com>
2026-02-19 19:31:55 +0530

    configuration

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('server running on port 500');
+import express from "express";
+
+import {PORT} from "./config/env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
